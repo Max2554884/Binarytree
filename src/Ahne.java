9dd1cffebaeb1ad9f51e4691f5860e5b0211997d
max2554884 <max.wurmbach@t-online.de>
2025-03-18 13:24:58 +0100

    grund struktur

--- src/Ahne.java
+++ src/Ahne.java
@@ -1,13 +1,35 @@
 public class Ahne {
-   private
+   private  String vorname,nachname;
+   private char geschlecht;
+
+   public void Ahne(String pVN, String pNN,char pG){
+     nachname = pNN;
+     vorname =  pVN;
+     geschlecht = pG;
+   }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
 
 
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
 
+    public char getGeschlecht() {
+        return geschlecht;
+    }
 
-
-
-
-
-
-
+    public void setGeschlecht(char geschlecht) {
+        this.geschlecht = geschlecht;
+    }
 }
